635346555182d678868c909631c0eb20b3d32d60
piyush ghevariya <pkghevariya15@gmail.com>
2018-09-07 19:46:23 +0530

    application yml setup

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,14 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "Order Tool"
-
-  # ==========development=================
-  config.api_key = '63600ce01a24e1db61267c3855b339a9'
-  config.secret = 'a60dee96ffb8f6ddeeb321d70a2ea3ff'
-
-  # ==========production=================
-  # config.api_key = 'c40fcdd4a8ec9a0d615a4e4307f6b7bf'
-  # config.secret = '3aacf0c182fc82056708baa9dd8ec43e'
-
+  config.api_key = ENV['shopify_api_key']
+  config.secret = ENV['shopify_api_secret']
   config.scope = "write_orders, write_products"
   config.embedded_app = true
   config.after_authenticate_job = false
