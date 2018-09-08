0119acfc710fe02a9ef94fb199701c8e824b8c82
pkghevariya <pkghevariya15@gmail.com>
2018-09-08 21:44:42 +0530

    add tracking no in job

--- app/jobs/order_update_job.rb
+++ app/jobs/order_update_job.rb
@@ -6,7 +6,7 @@ class OrderUpdateJob < ActiveJob::Base
     	
     	@order_status = webhook['fulfillment_status']
     	webhook['line_items'].each do |line_item|
-    		@is_digital = line_item["requires_shipping"]
+    		@is_digital = !line_item["requires_shipping"]
     	end
     	@shipping_company = webhook['shipping_lines'][0]['title'] rescue ''
     	@tracking_no = webhook['fulfillments'][0]['tracking_number'] rescue nil
@@ -18,16 +18,26 @@ class OrderUpdateJob < ActiveJob::Base
 			@order = ShopifyAPI::Order.find(webhook['id'])
       @order_updated = false
     	if @result.response.code == 200
+    		puts "Iffffffffffffffffffffffff==> order status updated to #{@order_status}"
+    		puts "Iff Order Note==#{@order.note}"
     		@order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
+    		@order_updated = true
     	else
+    		puts "else"
         for i in 0..1
           @result = shop.order_call(@order_status,@is_digital,@shipping_company,@tracking_no,@shop_domain)
           if @result.response.code == 200
+          	puts "elseeeeeeeeeeeeeeee==> order status updated to #{@order_status}"
+    				puts "elseeeeeeeeeee Order Note==#{@order.note}"
             @order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
             @order_updated = true
             break
           end
         end
+        puts "order_updated===#{@order_updated}"
+        puts "result=======#{@order.note != 'Error message here'}"
+        puts "result=======#{!@order_updated}"
+        puts "result=======#{@order.note != 'Error message here' && !@order_updated}"
     		@order.update_attributes(note: "Error message here") if @order.note != "Error message here" && !@order_updated
     	end
     end
