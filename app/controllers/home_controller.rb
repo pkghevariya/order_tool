e62db0dbc0817e6435e0fef1886a219b9321ef46
pkghevariya <pkghevariya15@gmail.com>
2018-09-08 08:04:05 +0530

    add httparty and coding in order_update webhook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,8 @@ class HomeController < ShopifyApp::AuthenticatedController
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
+
+  def update_order_status
+
+  end
 end
--- app/jobs/order_update_job.rb
+++ app/jobs/order_update_job.rb
@@ -3,6 +3,31 @@ class OrderUpdateJob < ActiveJob::Base
     shop = Shop.find_by(shopify_domain: shop_domain)
 
     shop.with_shopify_session do
+    	
+    	@order_status = webhook['fulfillment_status']
+    	webhook['line_items'].each do |line_item|
+    		@is_digital = line_item.requires_shipping
+    	end
+    	@shipping_company = webhook['shipping_lines'][0].title rescue ''
+    	# @tracking_no = not found
+    	@order_id = webhook['id']
+    	@shop_domain = shop_domain #unique identifier
+    	@result = HTTParty.post("https://order-tool.herokuapp.com/home/update_order_status", 
+	    :body => { :order_status => @order_status, 
+	               :is_digital => @is_digital, 
+	               :shipping_company => @shipping_company, 
+	               :tracking_no => @tracking_no, 
+	               :shop_domain => @shop_domain
+	             }.to_json,
+	    :headers => { 'Content-Type' => 'application/json' } )
+
+			#update order note
+			@order = ShopifyAPI::Order.find(webhook['id'])
+    	if @result
+    		@order.update(note: "order status updated to #{@order_status}")
+    	else
+    		@order.update(note: "Error message here")
+    	end
     end
   end
 end
