c262cb4945b0e22a4dbb69d19018bc44fec72e59
pkghevariya <pkghevariya15@gmail.com>
2018-09-08 21:55:03 +0530

    add tracking no in job

--- app/jobs/order_update_job.rb
+++ app/jobs/order_update_job.rb
@@ -5,6 +5,7 @@ class OrderUpdateJob < ActiveJob::Base
     shop.with_shopify_session do
     	
     	@order_status = webhook['fulfillment_status']
+    	@order_status = 'unfulfilled' unless @order_status.present?
     	webhook['line_items'].each do |line_item|
     		@is_digital = !line_item["requires_shipping"]
     	end
@@ -17,28 +18,18 @@ class OrderUpdateJob < ActiveJob::Base
 			#update order note
 			@order = ShopifyAPI::Order.find(webhook['id'])
       @order_updated = false
-      puts "@result.response.code===#{@result.response.code}===#{@result.response.code.class}"
     	if @result.response.code == '200'
-    		puts "Iffffffffffffffffffffffff==> order status updated to #{@order_status}"
-    		puts "Iff Order Note==#{@order.note}"
     		@order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
     		@order_updated = true
     	else
-    		puts "else"
         for i in 0..1
           @result = shop.order_call(@order_status,@is_digital,@shipping_company,@tracking_no,@shop_domain)
           if @result.response.code == '200'
-          	puts "elseeeeeeeeeeeeeeee==> order status updated to #{@order_status}"
-    				puts "elseeeeeeeeeee Order Note==#{@order.note}"
             @order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
             @order_updated = true
             break
           end
         end
-        puts "order_updated===#{@order_updated}"
-        puts "result=======#{@order.note != 'Error message here'}"
-        puts "result=======#{!@order_updated}"
-        puts "result=======#{@order.note != 'Error message here' && !@order_updated}"
     		@order.update_attributes(note: "Error message here") if @order.note != "Error message here" && !@order_updated
     	end
     end
