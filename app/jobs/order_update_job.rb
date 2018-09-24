f7c25af99b4e0f712db2ce46d0518791bc131192
piyush ghevariya <pkghevariya15@gmail.com>
2018-09-24 18:00:33 +0530

    call an api only when gateway-paypal and tracking info presents

--- app/jobs/order_update_job.rb
+++ app/jobs/order_update_job.rb
@@ -10,28 +10,33 @@ class OrderUpdateJob < ActiveJob::Base
     		@is_digital = !line_item["requires_shipping"]
     	end
     	@shipping_company = webhook['shipping_lines'][0]['title'] rescue ''
-    	@tracking_no = webhook['fulfillments'][0]['tracking_number'] rescue nil
+      @tracking_no = webhook['fulfillments'][0]['tracking_number'] rescue nil
+    	@tracking_company = webhook['fulfillments'][0]['tracking_company'] rescue nil
     	@order_id = webhook['id']
     	@shop_domain = shop_domain #unique identifier
-      @result = shop.order_call(@order_status,@is_digital,@shipping_company,@tracking_no,@shop_domain)
+      if webhook['gateway'].downcase == "paypal" && tracking_no.present? && @tracking_company.present?
+        @result = shop.order_call(@order_status,@is_digital,@shipping_company,@tracking_no,@shop_domain)
 
-			#update order note
-			@order = ShopifyAPI::Order.find(webhook['id'])
-      @order_updated = false
-    	if @result.response.code == '200'
-    		@order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
-    		@order_updated = true
-    	else
-        for i in 0..1
-          @result = shop.order_call(@order_status,@is_digital,@shipping_company,@tracking_no,@shop_domain)
-          if @result.response.code == '200'
-            @order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
-            @order_updated = true
-            break
+  			#update order note
+  			@order = ShopifyAPI::Order.find(webhook['id'])
+        @order_updated = false
+      	if @result.response.code == '200'
+      		@order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
+      		@order_updated = true
+      	else
+          for i in 0..1
+            @result = shop.order_call(@order_status,@is_digital,@shipping_company,@tracking_no,@shop_domain)
+            if @result.response.code == '200'
+              @order.update_attributes(note: "order status updated to #{@order_status}") if @order.note != "order status updated to #{@order_status}"
+              @order_updated = true
+              break
+            end
           end
-        end
-    		@order.update_attributes(note: "Error message here") if @order.note != "Error message here" && !@order_updated
-    	end
+      		@order.update_attributes(note: "Error message here") if @order.note != "Error message here" && !@order_updated
+      	end
+      else
+        puts "nothing happens"
+      end
     end
   end
 end
