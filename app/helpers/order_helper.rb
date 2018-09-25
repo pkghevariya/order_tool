898b73188e62997a4d432ed9b8e8bbdd2a190ec6
pkghevariya <pkghevariya15@gmail.com>
2018-09-25 23:37:41 +0530

    code indented

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -2,9 +2,9 @@ module OrderHelper
 	def order_status_change
 		@order_note = @order.note
 		['fulfilled','partial','restocked','unfulfilled'].each do |order_status|
-            @order_note = @order_note.gsub("order status updated to #{order_status}","")
-        end
-        @order.update_attributes(note: "#{@order_note} order status updated to #{@order_status}")
-        @order_updated = true
+			@order_note = @order_note.gsub("order status updated to #{order_status}","")
+		end
+		@order.update_attributes(note: "#{@order_note} order status updated to #{@order_status}")
+		@order_updated = true
 	end
 end
